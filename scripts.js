aca5ea2f30e82c91b539412b7db6cc97b8195715
Nanda Winata <41312762+ndawinata@users.noreply.github.com>
2019-11-02 15:57:36 +0700

    belajar events

--- scripts.js
+++ scripts.js
@@ -1,8 +1,19 @@
 $(document).ready(function () {
-    $('h1').css('color', 'red');
+    $('h1').css('color', 'blue');
     $('#box1').css('color', 'red');
     $('.boxes').css('color', 'blue');
     $('.boxes:first').css('color', 'green');
     $('.boxes:last').css('color', 'aqua');
     $('.boxes:eq(2)').css('color', 'purple');
-})
+
+    //menggunaklan events
+    $('h1').click(function () {
+        $('h1').css('color', 'red');
+    });
+    $('h1').mouseenter(function () {
+        $('h1').css('color', 'yellow');
+    });
+    $('h1').mouseleave(function () {
+        $('h1').css('color', 'green');
+    });
+});
